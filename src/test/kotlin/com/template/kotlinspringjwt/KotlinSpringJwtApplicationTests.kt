a67f97c257236a9565855bcb401224df273283dd
SongHae8640 <thdgo456@naver.com>
2025-02-12 17:07:22 +0900

    feat : 프로젝트 초기 세팅

---
+++ src/test/kotlin/com/template/kotlinspringjwt/KotlinSpringJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.template.kotlinspringjwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringJwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
